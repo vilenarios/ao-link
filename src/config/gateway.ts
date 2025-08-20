10ba6ed9c2f306f431e641b996468256dfaec96b
smowden <smowden@users.noreply.github.com>
2025-08-20 18:58:12 +0200

    Update gateway.ts

--- src/config/gateway.ts
+++ src/config/gateway.ts
@@ -1,2 +1,2 @@
-export const GATEWAY_GRAPHQL = "https://arweave-search.goldsky.com/graphql"
+export const GATEWAY_GRAPHQL = "https://ao-search-gateway.goldsky.com/graphql"
 export const GATEWAY_DATA = "https://arweave.net"
